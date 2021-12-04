7903aca6e36d3249bb4e5460fb68ed729e5e1abd
Chun Xuan <56767365+chunxuan98@users.noreply.github.com>
2021-12-04 23:41:42 +0800

    Update package_application_withrealcase.R

--- vignettes/package_application_withrealcase.R
+++ vignettes/package_application_withrealcase.R
@@ -9,15 +9,16 @@ lapply(packsneed, require, character.only = TRUE)
 #define wrapper function for reading the mrm files
 #funciton definition: read mrm file with default experiment setting and allow noise parameters imput  #done
 #function param
-filedat <- list.files(path = 'dmrm/', pattern = ".mzML$", full.names = TRUE)
+filedat <- list.files(path = 'datasets/', pattern = ".mzML$", full.names = TRUE)
 pwid <- c(0.8,3)
-snthr <- 10
+snthr<- 10
 MSRT <- read.csv("MS1_RT.csv", col.names = c('MS1','RT'))
-rtfile <- data.frame("row"=1:length(rt),"ms1"=MSRT$MS1, "predrt"=MSRT$RT)
+rtfile <- data.frame("row"=1:length(MSRT$RT),"ms1"=MSRT$MS1, "predrt"=MSRT$RT)
 delrt <- 1.5
 group1 <- c(1:3)
 group2 <- c(4:5)
 peaksresult <- peaksdetect(filedat, rtfile,pwid, snthr, delrt, group1,group2)
+peaksresult
 
 #load new data for rt prediction  #done
 newinput <- read.csv('OH_newdata_predictionRT.csv')
