a271a157110f62c8f3fdd3b00a14fa626505b217
Chun Xuan <56767365+chunxuan98@users.noreply.github.com>
2021-12-05 00:01:00 +0800

    Update package_application_withrealcase.R

--- vignettes/package_application_withrealcase.R
+++ vignettes/package_application_withrealcase.R
@@ -32,3 +32,38 @@ num <- 50
 m <- 171.1 #for OH 171.1, for cooh 136.1
 t <- 1000 #threshold in agilent experiment settings format 
 groupresult <- prepare_data(file, num, m, t)
+
+
+
+########################################
+
+#load smiles of molecules from OH compounds
+rtdata <- read.csv("OH_RT_zj.csv")
+smilst <- rtdata$smiles
+rt <- rtdata$RT
+#model start training
+datadescs <- getdesc(smilst)
+norm_data <- getnorm(datadescs,rt)
+OH_model <- getmodel(norm_data, picname = 'OH_data')
+
+#load smiles of molecules from COOH compounds
+rtdata2 <- read.csv("COOH_RT_zj.csv")
+smilst2 <- rtdata2$smiles
+rt2 <- rtdata2$RT
+#model start training
+datadescs2 <- getdesc(smilst2)
+norm_data2 <- getnorm(datadescs2,rt2)
+COOH_model <- getmodel(norm_data2, picname = 'COOH_data')
+
+#save model settings
+OH_descs <- colnames(datadescs) #descripotrs before norm
+OH_descnorsettings <- getnormsettings(datadescs)
+COOH_descs <- colnames(datadescs2) #descriptors before norm
+COOH_descnorsettings <- getnormsettings(datadescs2)
+
+save(OH_model,file = 'OH_model.RData')
+save(OH_descs,file = 'OH_descs.RData')
+save(OH_descnorsettings,file = 'OH_normsettings.RData')
+save(COOH_model,file = 'COOH_model.RData')
+save(COOH_descs,file = 'COOH_descs.RData')
+save(COOH_descnorsettings,file = 'COOH_normsettings.RData')
